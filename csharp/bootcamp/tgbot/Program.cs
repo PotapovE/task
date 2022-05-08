de707b0ca6316d3bbdbc294f4597268acd16f88c
PotapovE <potapovomsk@gmail.com>
2022-05-08 22:54:33 +0300

    Парсим бота в тг Bootcamp

--- csharp/bootcamp/tgbot/Program.cs
+++ csharp/bootcamp/tgbot/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://api.telegram.org/bot<token>/getUpdates
+// https://api.telegram.org/bot<token>/sendMessage?chat_id=1852284449&text=How%20are%20u?
+
+using Newtonsoft.Json.Linq;
+
+string url = "https://api.telegram.org/bot<token>/getUpdates";
+HttpClient hc = new HttpClient();
+string res = hc.GetStringAsync(url).Result;
+JObject resultReq = JObject.Parse(res);
+JToken result = resultReq["result"]!;
+
+foreach (var item in result)
+{
+    string fName = item["message"]!["from"]!["first_name"]!.ToString();
+    string text = item["message"]!["text"]!.ToString();
+    Console.WriteLine($"{fName}: {text}");
+}
