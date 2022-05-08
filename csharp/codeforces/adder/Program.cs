f33ce1d649ccdc503fe44cf7ac25648889e7ad55
PotapovE <potapovomsk@gmail.com>
2022-05-08 14:13:42 +0300

    Сумматор codeforces

---
+++ csharp/codeforces/adder/Program.cs
@@ -0,0 +1,8 @@
+﻿// https://route256.contest.codeforces.com/group/OcTypf5d5d/contest/380037/problem/A
+
+int n = Convert.ToInt32(Console.ReadLine())!;
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine()!.Split(' ');
+    Console.WriteLine(Convert.ToInt32(s[0]) + Convert.ToInt32(s[1]));
+}
