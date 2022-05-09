6929930b024ebb6fadaa6eb05a6cbaf950564747
PotapovE <potapovomsk@gmail.com>
2022-05-09 18:25:42 +0300

    Модель сообщений в тг Bootcamp

--- csharp/bootcamp/tgbot/Program.cs
+++ csharp/bootcamp/tgbot/Program.cs
@@ -1,9 +1,7 @@
-﻿// See https://api.telegram.org/bot<token>/getUpdates
-// https://api.telegram.org/bot<token>/sendMessage?chat_id=1852284449&text=How%20are%20u?
+﻿using Newtonsoft.Json.Linq;
 
-using Newtonsoft.Json.Linq;
-
-string url = "https://api.telegram.org/bot<token>/getUpdates";
+string token = File.ReadAllText(@"D:\programming\Test\token");
+string url = $"https://api.telegram.org/bot{token}/getUpdates";
 HttpClient hc = new HttpClient();
 string res = hc.GetStringAsync(url).Result;
 JObject resultReq = JObject.Parse(res);
@@ -12,6 +10,6 @@ JToken result = resultReq["result"]!;
 foreach (var item in result)
 {
     string fName = item["message"]!["from"]!["first_name"]!.ToString();
-    string text = item["message"]!["text"]!.ToString();
+    string? text = item["message"]!["text"]?.ToString();
     Console.WriteLine($"{fName}: {text}");
 }
