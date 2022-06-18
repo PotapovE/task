8b54d7473acff42fcb25b85c8fba7017137d6740
PotapovE <potapovomsk@gmail.com>
2022-06-18 12:01:18 +0700

    Тестовый контроллер

---
+++ csharp/MyMvcUI/MyMvcUI/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MyMvcUI.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "This is good";
+        }
+        public string Welcome(string name, int ID)
+        {
+            return HtmlEncoder.Default.Encode($"Hi {name}! ID: {ID}");
+        }
+    }
+}
